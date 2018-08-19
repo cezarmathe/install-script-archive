470bfafcdaa93078b35fefd027cc78a0d2e32e87
cezarmathe <cezarmathe@gmail.com>
2018-08-19 17:56:25 +0300

    testing

--- install.sh
+++ install.sh
@@ -28,32 +28,6 @@ pacstrap /mnt base base-devel
 echo "Generating fs tab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Changing root"
-arch-chroot /mnt
-
-echo "Setting local time"
-ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
-
-echo "Syncing hwclock"
-hwclock --systohc
-
-echo "Localization"
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-echo "Network configuration"
-echo "arch-pc" > /etc/hostname
-echo "127.0.0.1       localhost" > /etc/hosts
-echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       arch-pc.localdomain       arch-pc"
-
-echo "Change root password:"
-passwd
-
-echo "Adding a user"
-useradd --create-home -aG power,storage,wheel cezar
-passwd cezar
-
-echo "Setting sudo"
-echo "%wheel ALL=(ALL) ALL"
+cp packages.txt /mnt
+cp post-install.sh /mnt
+arch-chroot /mnt post-install.sh
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+wget https://raw.githubusercontent.com/cezarmathe/install-script/master/post-install.sh
+wget https://raw.githubusercontent.com/cezarmathe/install-script/master/packages.txt
+
+bash "${curl -fsSl }"
