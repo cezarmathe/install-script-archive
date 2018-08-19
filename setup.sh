3b03f686eddc379307d315dace868025f46994ca
cezarmathe <cezarmathe@gmail.com>
2018-08-19 18:09:42 +0300

    test3

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 wget https://raw.githubusercontent.com/cezarmathe/install-script/master/post-install.sh
 wget https://raw.githubusercontent.com/cezarmathe/install-script/master/packages.txt
 
-bash "${curl -fsSl https://raw.githubusercontent.com/cezarmathe/install-script/master/install.sh}"
+bash "$(curl -fsSl https://raw.githubusercontent.com/cezarmathe/install-script/master/install.sh)"
